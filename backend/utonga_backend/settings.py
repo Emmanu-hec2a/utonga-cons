526bcebe4e2907ab8ad842909261767e323440ee
Emmanuel Odongo <odiwourmanu123@gmail.com>
2026-08-10 15:01:21 +0300

    Update: Weather

--- backend/utonga_backend/settings.py
+++ backend/utonga_backend/settings.py
@@ -180,3 +180,6 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
 TWILIO_FROM_NUMBER = os.environ.get('TWILIO_FROM_NUMBER', '')
 
+# Weather Configuration
+OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY', '')
+

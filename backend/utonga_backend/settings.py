4f1ccff0c9ed0a5cc9912bbc41958c2efedfd573
Emmanuel Odongo <odiwourmanu123@gmail.com>
2026-09-17 09:48:55 +0300

    Updates: New Updates

--- backend/utonga_backend/settings.py
+++ backend/utonga_backend/settings.py
@@ -12,7 +12,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-rfzsl8g#c7he_s7^9)w-v4=8hvzmo%$^s&o=_8tzo7$nnl3omg')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
